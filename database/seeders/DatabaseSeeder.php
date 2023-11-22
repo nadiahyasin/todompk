690716cb18ce1fc2c8d684b047d67d7b38c3dd4c
Nadiahyasin <nadiah.matyasin@gmail.com>
2023-11-22 11:48:20 +0800

    seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(200)->create();
+         \App\Models\Post::factory(300)->create();
+         \App\Models\Comment::factory(500)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
